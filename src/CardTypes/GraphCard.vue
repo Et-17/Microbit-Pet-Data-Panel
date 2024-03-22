fcf9529baf14b3bbd4eb50137f1bf25b630d9d9a
Luna Kilborn <leland.kilborn@hotmail.com>
2024-03-21 22:49:44 -0400

    i want to cry

--- src/CardTypes/GraphCard.vue
+++ src/CardTypes/GraphCard.vue
@@ -2,36 +2,31 @@
 import { onMounted, ref } from 'vue';
 import CardBase from './CardBase.vue';
 import { listeners, type Message } from '@/receiver_management/message_handling';
-import Chart from 'chart.js/auto'
-
-const body_text = ref("hi");
-let current_label_counter = 0;
+// i know i'm not supposed to do this but i'm too busy and lazy to figure out
+// the fancy tree shaking stuff
+import Chart from 'chart.js/auto';
 
 function receive(num: number) {
   if (chart == undefined || chart.data.labels == undefined) {
     return;
   }
-  // console.log(chart.data.datasets[0].data);
-  // console.log(chart.data.labels)
-  // chart.data.datasets[0].data.push(num);
-  // chart.data.labels.push((chart.data.labels[chart.data.labels.length - 1] ?? 0) + 1);
-  // if (chart.data.datasets[0].data.length > props.max_data_points) {
-  //   chart.data.datasets[0].data.splice(3, 1);
-  //   chart.data.labels.splice(3, 1);
-  // }
-  // chart.update();
 
-  // chart.data.labels.push(current_label_counter);
-  // chart.data.datasets[0].data.push({ x: current_label_counter, y: num });
-  // current_label_counter += 1;
-  // if (chart.data.datasets[0].data.length > props.max_data_points) {
-  //   chart.data.labels.shift();
-  // }
-
-  for (let i = 0; i < chart.data.datasets[0].data.length - 1; i++) {
-    chart.data.datasets[0].data[i] = chart.data.datasets[0].data[i + 1];
+  if (chart.data.datasets[0].data.length > props.max_data_points) {
+    chart.data.datasets[0].data.shift();
+    chart.update();
+  } else if (chart.data.datasets[0].data.length < props.max_data_points) {
+    // i'm not entirely sure why i have to cast to a number here but it works
+    chart.data.labels.push((chart.data.labels[chart.data.labels.length - 1] as number ?? 0) + 1);
   }
-  chart.data.datasets[0].data[chart.data.datasets[0].data.length] = num;
+  chart.data.datasets[0].data.push(num);
+  chart.update();
+}
+
+function remove_point() {
+  if (chart == undefined || chart.data.labels == undefined) {
+    return;
+  }
+  chart.data.datasets[0].data.shift();
   chart.update();
 }
 
@@ -42,39 +37,24 @@ onMounted(() => {
   if (graph.value == undefined) {
     return;
   }
-  // chart = new Chart(graph.value, {
-  //   type: "bar",
-  //   data: {
-  //     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  //     datasets: [{
-  //       label: "# of Votes",
-  //       data: [12, 19, 3, 5, 2, 3],
-  //       borderWidth: 1
-  //     }]
-  //   },
-  //   options: {
-  //     scales: {
-  //       y: {
-  //         beginAtZero: true
-  //       }
-  //     }
-  //   }
-  // });
   chart = new Chart(graph.value, {
     type: "line",
     data: {
       datasets: [{
-        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        data: []
       }],
-      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      labels: []
     },
-    // options: {
-    //   scales: {
-    //     x: {
-    //       display: false
-    //     }
-    //   }
-    // }
+    options: {
+      scales: {
+        y: {
+          type: "linear",
+          position: "left",
+          max: props.max_value,
+          min: props.min_value
+        },
+      },
+    },
   })
 })
 
@@ -94,5 +74,6 @@ const props = defineProps<{
 <template>
   <CardBase :cs="cs" :rs="rs" :ce="ce" :re="re" :listeningKey="listeningKey" @new-value="receive">
     <canvas ref="graph"></canvas>
+    <button @click="remove_point">remove</button>
   </CardBase>
 </template>
